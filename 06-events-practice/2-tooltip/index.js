9bb4cc59482d5dbb2ff4f492f900d0467fd0b7a1
Anisimov Artem <a.anisimov@runetsoft.ru>
2022-04-07 15:04:44 +0300

    Tooltip компонент

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,51 @@
 class Tooltip {
-  initialize () {
+  static instance;
 
+  element;
+
+  onPointerOver = event => {
+    const element = event.target.closest('[data-tooltip]');
+
+    if (element) {
+      this.render(element.dataset.tooltip);
+      document.addEventListener('pointermove', this.onPointerMove);
+    }
+  };
+
+  onPointerMove = event => {
+    this.moveTooltip(event);
+  };
+
+  onPointerOut = () => {
+    this.remove();
+    document.removeEventListener('pointermove', this.onPointerMove);
+  };
+
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+
+    Tooltip.instance = this;
   }
+
+  initialize() {
+    this.initEventListeners();
+  }
+
+  initEventListeners() {
+    document.addEventListener('pointerover', this.onPointerOver);
+    document.addEventListener('pointerout', this.onPointerOut);
+  }
+
+  render(html) {
+    this.element = document.createElement('div');
+    this.element.className = 'tooltip';
+    this.element.innerHTML = html;
+
+    document.body.append(this.element);
+  }
+
 }
 
 export default Tooltip;
